0e3d7a981a3c438b891819214b8eded056da16ac
gjohnsx <gregjohns@fastmail.com>
2022-06-11 00:35:01 -0400

    Updated footer links

--- projects/scrimba/module13/solo-project/src/components/Footer.js
+++ projects/scrimba/module13/solo-project/src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer className='footer'>
-            <i className="footer-icon fa-brands fa-twitter-square"></i>
-            <i className="footer-icon fa-brands fa-github-square"></i>
-            <i className="footer-icon fa-brands fa-linkedin"></i>
+            <a href="https://twitter.com/gjohnsx" target="_blank"><i className="footer-icon fa-brands fa-twitter-square"></i></a>
+            <a href="https://github.com/gjohnsx" target="_blank"><i className="footer-icon fa-brands fa-github-square"></i></a>
+            <a href="https://linkedin.com/in/greg-johns" target="_blank"><i className="footer-icon fa-brands fa-linkedin"></i></a>
         </footer>
     )
 }
